35f027f6b05b832fe535eac1faa022f23615eda3
Dev-Souza <kauan.souza.dev@gmail.com>
2025-06-13 11:30:42 -0300

    feat: component loading

---
+++ src/components/utils/LoadingComponent.jsx
@@ -0,0 +1,7 @@
+export default function LoadingComponent() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <span className="loading loading-bars loading-xl"></span>
+        </div>
+    );
+}
